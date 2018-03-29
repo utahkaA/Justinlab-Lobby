018879dbd65f172bad4b476fefe07289c0fa08b4
utahkaA <utahka.akiba@gmail.com>
2018-03-29 15:24:02 +0900

    [update] updated main.py

--- main.py
+++ main.py
@@ -3,10 +3,8 @@ import binascii
 import requests
 import json
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from db.session import Session
 import nfc
-from models.users import User
 
 def ctrlc_handler(signal, frame):
   print("\nKeyboardInterrupt")
@@ -40,31 +38,21 @@ class CardReader():
 
 def notify(session, info):
   idm = info["idm"]
-  match_users = session.query(User).filter(
-    User.idm == idm
-  ).all()
-  target_user = match_users[0]
-  requests.post(target_user.webhook_url, data=json.dumps({
-    "text": "[Test] Touch and Go",
-    "username": "Justinlab",
-    "icon_emoji": ":gopher",
-    "link_names": 1,
-  }))
+  with Session() as sess:
+    match_users = sess.query(User).filter(
+      User.idm == idm
+    ).all()
+    target_user = match_users[0]
+    requests.post(target_user.webhook_url, data=json.dumps({
+      "text": "[Test] Touch and Go",
+      "username": "Justinlab",
+      "icon_emoji": ":gopher",
+      "link_names": 1,
+    }))
 
 def _main():
   # for Ctrl-C
   signal.signal(signal.SIGINT, ctrlc_handler)
-
-  # for Database
-  db_url = "mysql+pymysql://root:mariadb@0.0.0.0:3306/justinlab?charset=utf8"
-  engine = create_engine(db_url)
-  session = scoped_session(
-    sessionmaker(
-      autocommit = False,
-      autoflush = False,
-      bind = engine
-    )
-  )
 
   target = 'usb:054c:02e1'
   reader = CardReader(target)
@@ -72,7 +60,8 @@ def _main():
     reader.ready()
     info = reader.get_info()
     if info is not None:
-      notify(session, info)
+      # notify(session, info)
+      print(info)
 
 if __name__ == "__main__":
   _main()
